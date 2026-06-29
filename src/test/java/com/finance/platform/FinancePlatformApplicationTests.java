38b765ead0311ea3be8682f541e0c36bf82376f4
Vithursan <Vithursant@pepulux.com>
2026-06-29 14:18:57 +0530

    New Java Project

---
+++ src/test/java/com/finance/platform/FinancePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
